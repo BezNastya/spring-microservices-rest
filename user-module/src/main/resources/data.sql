8d639a93e9ce1a2de330441908157701210bd6e8
BlackKyiv <62792985+BlackKyiv@users.noreply.github.com>
2022-11-06 18:38:29 +0100

    Fixed Kate's config

---
+++ user-module/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, login, password, age) VALUES (1,'Anna','pass', 15);
+INSERT INTO users (id, login, password, age)  VALUES (2,'Mick','pass', 20);
+INSERT INTO users (id, login, password, age) VALUES (3,'Ben','pass', 35);
