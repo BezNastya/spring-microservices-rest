f56cd5cd4efa3d45be60f2b6e754dd79454023cc
BlackKyiv <62792985+BlackKyiv@users.noreply.github.com>
2022-12-04 16:51:39 +0100

    Added Admin server and added configuration for client modules

---
+++ admin-module/src/test/java/com/example/adminmodule/AdminModuleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.adminmodule;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AdminModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
