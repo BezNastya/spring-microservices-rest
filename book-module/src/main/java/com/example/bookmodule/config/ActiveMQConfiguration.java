714343f15dbf39e1d7bfbb25102acba606e457aa
KateKolokhina <39592816+KateKolokhina@users.noreply.github.com>
2022-12-04 19:05:28 +0200

    Finished PtoP part

--- book-module/src/main/java/com/example/bookmodule/config/ActiveMQConfiguration.java
+++ book-module/src/main/java/com/example/bookmodule/config/ActiveMQConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.bookmodule.config;
 
 import com.example.bookmodule.dto.BookOrderDTO;
+import com.example.bookmodule.dto.BookRequestDTO;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.region.policy.PolicyEntry;
 import org.apache.activemq.broker.region.policy.PolicyMap;
@@ -15,6 +16,7 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @EnableJms
@@ -24,6 +26,9 @@ public class ActiveMQConfiguration {
     public static final String ORDER_QUEUE = "book_orders";
     public static final String AUTHOR_QUEUE = "author_queue";
     public static final String USERS_BOOK_QUEUE = "USERS_BOOK_QUEUE";
+    public static final String BOOK_QUEUE = "new_book";
+    public static final String BOOK_WITH_AUTHOR_QUEUE = "book_author";
+
     public static final String NEW_ORDER_JMS_TYPE = "NEW";
     public static final String CANCEL_ORDER_JMS_TYPE= "CANCEL";
     public static final String DELETE_ORDER_JMS_TYPE= "DELETE";
@@ -36,11 +41,12 @@ public class ActiveMQConfiguration {
         return factory;
     }
 
-    @Bean
+    @Bean(name = "converter")
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         Map<String, Class<?>> typeIdMappings = new HashMap<>();
         typeIdMappings.put("JMS_TYPE", BookOrderDTO.class);
+        typeIdMappings.put("Test", BookRequestDTO.class);
         converter.setTypeIdMappings(typeIdMappings);
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
